f972b465ac94f6c0072626acf14ee87eeda87734
nerdymedic <mgoebel@gmail.com>
2020-05-22 21:26:28 -0400

    added parameter to specificy cc/kg

--- graph.R
+++ graph.R
@@ -1,18 +1,5 @@
 library(tidyverse)
 
-
-data <- seq(58, 78, 1) %>% 
-  tibble() %>% 
-  select("height" = 1) %>% 
-  mutate(male_wt = 50 + 2.3*(height - 60), female_wt = 45.5 + 2.3*(height - 60)) %>% 
-  mutate(male_tv = 6*male_wt, female_tv = 6*female_wt)
-
-data %>% 
-  ggplot(aes(x = height)) +
-  geom_point(aes(y = male_tv), col = 'blue')+
-  geom_point(aes(y = female_tv), col = "pink") +
-  labs(x = "Height (in)", y = "Volume (ml)", title = "6mL/kg TV by height")
-
 male_mean_height <- 70
 male_sd_height  <- 3
 female_mean_height <- 64.5
@@ -21,7 +8,23 @@ female_sd_height <- 2.5
 male_empiric_tv <- 450
 female_empiric_tv <- 350
 no_SD <- 1
+cc_per_kg <- 6
 
+#generate ideal bw and tv data
+data <- seq(58, 78, 1) %>% 
+  tibble() %>% 
+  select("height" = 1) %>% 
+  mutate(male_wt = 50 + 2.3*(height - 60), female_wt = 45.5 + 2.3*(height - 60)) %>% 
+  mutate(male_tv = cc_per_kg*male_wt, female_tv = cc_per_kg*female_wt)
+
+#ideal tv plot
+data %>% 
+  ggplot(aes(x = height)) +
+  geom_point(aes(y = male_tv), col = 'blue')+
+  geom_point(aes(y = female_tv), col = "pink") +
+  labs(x = "Height (in)", y = "Volume (ml)", title = paste0(cc_per_kg,"mL/kg TV by height"))
+
+#tv error plot
 data %>% 
   mutate(error_m = male_empiric_tv - male_tv, error_f = female_empiric_tv - female_tv) %>% 
   ggplot(aes(x = height))  +
@@ -29,7 +32,7 @@ data %>%
   geom_point(aes(y = error_f), col = "pink") +
   labs(x = "Height (in)", 
        y = "Volume Error (cc)", 
-       title = paste0("Error of empiric TV ","(",male_empiric_tv,"cc male, ", female_empiric_tv,"cc female) vs 6cc/kg"), 
+       title = paste0("Error of Empiric TV ","(",male_empiric_tv,"cc male, ", female_empiric_tv,"cc female) vs ",cc_per_kg,"cc/kg"), 
        subtitle = "by @nerdymedic",
        caption = paste("Dotted lines are",no_SD,"SD of height")) +
   geom_vline(xintercept = c(male_mean_height - no_SD * male_sd_height, 
@@ -41,5 +44,4 @@ data %>%
              color = "pink", 
              linetype = "dotted") +
   theme_bw()
-  
 
